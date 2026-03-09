25117d0a694dab481c3d63d15de972953c82a05a
Govindaraj606 <govindarajk8092@gmail.com>
2026-03-09 20:27:04 +0530

    homepage and navbar changes

--- src/app/page-form/page-form.ts
+++ src/app/page-form/page-form.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-page-form',
@@ -12,6 +13,10 @@ export class PageForm {
   contactForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
+
+  
+    emailjs.init("YOUR_PUBLIC_KEY");
+
     this.contactForm = this.fb.group({
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
@@ -19,14 +24,49 @@ export class PageForm {
       email: ['', [Validators.required, Validators.email]],
       message: ['', [Validators.required]]
     });
+
   }
 
+
   submitForm() {
+
     if (this.contactForm.valid) {
-      console.log(this.contactForm.value);
-      this.contactForm.reset();
+
+      const formData = this.contactForm.value;
+
+      const templateParams = {
+        firstName: formData.firstName,
+        lastName: formData.lastName,
+        phone: formData.mobile,
+        email: formData.email,
+        message: formData.message
+      };
+
+      emailjs.send(
+        "service_xxxxxx",   
+        "template_xxxxxx",
+        templateParams
+      )
+        .then(() => {
+
+          alert("Message sent successfully!");
+
+          this.contactForm.reset();
+
+        })
+        .catch((error) => {
+
+          console.error("EmailJS Error:", error);
+
+          alert("Failed to send message");
+
+        });
+
     } else {
+
       this.contactForm.markAllAsTouched();
+
     }
+
   }
 }
